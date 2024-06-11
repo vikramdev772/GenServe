527c2628a37d18b5452a7281fd81274664f5908d
vikram <vikramdev772@gmail.com>
2024-06-11 16:36:54 +0530

    ��

--- client/src/Auth/Login.jsx
+++ client/src/Auth/Login.jsx
@@ -1,63 +1,67 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Navbar from "../screens/Navbar";
 
 const Login = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="max-w-md relative flex flex-col p-4 rounded-md text-black bg-white">
-        <div className="text-2xl font-bold mb-2 text-[#1e0e4b] text-center">
-          Welcome back to <span className="text-[#7747ff]">Data Care</span>
-        </div>
-        <div className="text-sm font-normal mb-4 text-center text-[#1e0e4b]">
-          Log in to your account
-        </div>
-        <form className="flex flex-col gap-3">
-          <div className="block relative">
-            <label
-              htmlFor="email"
-              className="block text-gray-600 cursor-text text-sm leading-[140%] font-normal mb-2"
+    <>
+      <Navbar />
+      <div className="flex items-center justify-center h-screen bg-black">
+        <div className="max-w-md relative flex flex-col p-10 rounded-md text-white bg-gray-800">
+          <div className="text-2xl font-bold mb-2 text-white text-center">
+            Welcome back to <span className="text-blue-400">Data Care</span>
+          </div>
+          <div className="text-sm font-normal mb-4 text-center text-gray-400">
+            Log in to your account
+          </div>
+          <form className="flex flex-col gap-3">
+            <div className="block relative">
+              <label
+                htmlFor="email"
+                className="block text-gray-400 cursor-text text-sm leading-[140%] font-normal mb-2"
+              >
+                Email
+              </label>
+              <input
+                type="text"
+                id="email"
+                className="rounded border border-gray-600 text-sm w-full font-normal leading-[18px] text-white bg-gray-700 tracking-[0px] appearance-none block h-11 m-0 p-[11px] focus:ring-2 ring-offset-2 ring-gray-500 outline-0"
+              />
+            </div>
+            <div className="block relative">
+              <label
+                htmlFor="password"
+                className="block text-gray-400 cursor-text text-sm leading-[140%] font-normal mb-2"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="rounded border border-gray-600 text-sm w-full font-normal leading-[18px] text-white bg-gray-700 tracking-[0px] appearance-none block h-11 m-0 p-[11px] focus:ring-2 ring-offset-2 ring-gray-500 outline-0"
+              />
+            </div>
+            <div>
+              <a className="text-sm text-blue-400" href="#">
+                Forgot your password?
+              </a>
+            </div>
+            <button
+              type="submit"
+              className="bg-blue-400 w-max m-auto px-6 py-2 rounded text-white text-sm font-normal"
             >
-              Email
-            </label>
-            <input
-              type="text"
-              id="email"
-              className="rounded border border-gray-200 text-sm w-full font-normal leading-[18px] text-black tracking-[0px] appearance-none block h-11 m-0 p-[11px] focus:ring-2 ring-offset-2 ring-gray-900 outline-0"
-            />
+              Submit
+            </button>
+          </form>
+          <div className="text-sm text-center mt-[1.6rem] text-gray-400">
+            Don’t have an account yet?{" "}
+            <Link className="text-sm text-blue-400" to="/signup">
+              Sign up!
+            </Link>
           </div>
-          <div className="block relative">
-            <label
-              htmlFor="password"
-              className="block text-gray-600 cursor-text text-sm leading-[140%] font-normal mb-2"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="rounded border border-gray-200 text-sm w-full font-normal leading-[18px] text-black tracking-[0px] appearance-none block h-11 m-0 p-[11px] focus:ring-2 ring-offset-2 ring-gray-900 outline-0"
-            />
-          </div>
-          <div>
-            <a className="text-sm text-[#7747ff]" href="#">
-              Forgot your password?
-            </a>
-          </div>
-          <button
-            type="submit"
-            className="bg-[#7747ff] w-max m-auto px-6 py-2 rounded text-white text-sm font-normal"
-          >
-            Submit
-          </button>
-        </form>
-        <div className="text-sm text-center mt-[1.6rem]">
-          Don’t have an account yet?{" "}
-          <Link className="text-sm text-[#7747ff]" to="/signup">
-            Sign up !
-          </Link>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
